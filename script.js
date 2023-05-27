84805694c97c0c2c0981e1ea1a38a31b164874c6
marsseptiani <134788752+marsseptiani@users.noreply.github.com>
2023-05-27 20:40:25 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Script for navigation bar
+// const bar = document.getElementById('bar')
+// const nav = document.getElementById('navbar')
+
+// if(bar){
+//     bar.addEventListener('onclick'),() => {
+//         nav.classList.add('active')
+//     }
+// }
+
+function tampilkanPesan() {
+    alert("Halo! Selamat datang di toko baju.");
+  }
+
+  function tampilkan() {
+    alert("Pastikan alamat e-mail sudah benar ya!");
+  }
+
+  
